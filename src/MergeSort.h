b7b0b09a3295994b3f5c2f97ff0b3d25b757856a
Sebastian Godinez <sebastian.godinez@sansano.usm.cl>
2018-12-04 18:32:24 -0300

    subi el makefile

--- src/MergeSort.h
+++ src/MergeSort.h
@@ -1,56 +1,7 @@
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
-#include <stdio.h>
-#include <math.h>
-#include <iostream>
-
-using namespace std;
-
-class avion{
-    private:
-        float x;
-        float y;
-    public:
-        avion();
-        avion(float x, float y);
-        ~avion();
-        float getX();
-        float getY();
-};
-
-avion::avion(){
-    this->x = 0;
-    this->y = 0;
-}
-
-avion::avion(float x, float y){
-    this->x = x;
-    this->y = y;
-}
-
-avion::~avion(){
-}
-
-float avion::getX(){
-    return this->x;
-}
-
-float avion::getY(){
-    return this->y;
-}
-float distancia(avion a, avion b){
-    float A = pow((a.getX() - b.getX()),2);
-    float B = pow((a.getY() - b.getY()),2);
-    
-    return pow(A+B,(1/2)); 
-}
-
-void printArreglo(avion *A, int l){
-    for(int i = 0; i < l; i++){
-        cout << "Valor de x: " << A[i].getX() << " Valor de y: " << A[i].getY() << endl;
-    }
-}
+#include "Avion.h"
 
 void mergeSort(avion *A, avion *temp, int izq, int der){
     int i, i1, i2, curr, med = (izq + der)/2;
