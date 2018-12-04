6a0181422e52a16398e0a6b8b3c9e15b10728c50
daniel alejandro toro vega <daniel.toro@lds-pc17.inf.santiago.usm.cl>
2018-12-04 18:15:41 -0300

    main loop

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,11 @@ using namespace std;
 main(int argc, char const *argv[]){
     avion *A = new avion[10];
     avion *temp = new avion[10];
-    for(int i = 0; i < 10; i++)
-    {
-        A[i] = avion((i*7+13)%4,i+1);
-    }
+    
+    int s; 
+	while(cin >> s){
+		cout << s << endl;
+	}
 
-    printArreglo(A, 10);
-
-    mergeSort(A, temp, 0, 9);
-    cout << "DESPUES" << endl;
-    for(int i = 0; i < 10; i++)
-    {
-        cout << "Valor de x: " << A[i].getX() << " Valor de y: " << A[i].getY() << endl;
-    }
     return 0;
 }
