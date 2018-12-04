b59ef04e66cc44af21e6ab4868889365ab186883
Sebastian Godinez <sebastian.godinez@sansano.usm.cl>
2018-12-04 19:18:54 -0300

    agregue la clase auxiliar

--- src/DividirConquistar.h
+++ src/DividirConquistar.h
@@ -1,6 +1,50 @@
 #include "Avion.h"
 #include "MergeSort.h"
+#include <iostream>
 
-avion *cercanos(avion *A, int n){
-    mergeSort(A, new avion[n], 0, n);
+using namespace std;
+class cercanos{
+    private:
+        avion A;
+        avion B;
+        float distancia;
+    public:
+        cercanos();
+        ~cercanos();
+        void setValores(avion, avion, float);
+        float getDistancia();
+        void printValues();
+
+};
+
+cercanos::cercanos(){
+    A = avion();
+    B = avion();
+}
+
+cercanos::~cercanos(){
+    // delete
+}
+
+void cercanos::setValores(avion X, avion Y, float l){
+    this->A = X;
+    this->B = Y;
+    distancia = l;
+}
+
+float cercanos::getDistancia(){
+    return this->distancia;
+}
+
+void cercanos::printValues(){
+    cout << A.getX() << " " << A.getY() << endl << B.getX() << " " << B.getY() << endl; 
+}
+
+cercanos *cercaaux(){
+    return NULL;
+}
+
+cercanos *cerca(avion *A, int n){
+    mergeSort(A, n);
+    return NULL;
 }
